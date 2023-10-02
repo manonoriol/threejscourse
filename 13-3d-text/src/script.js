169f67963794a41aac37617045894d4d41102949
Manon Oriol <oriol.manon@gmail.com>
2023-10-02 21:55:24 +0200

    small modifications

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -2,10 +2,15 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+// import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+
 
 /**
  * Base
  */
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,22 +18,87 @@ const gui = new dat.GUI()
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+//Axes helper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
 
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load('textures/matcaps/3.png');
+const matcapTextureMetal = textureLoader.load('textures/matcaps/9.png');
+
+/**
+ * Fonts
+ */
+
+const fontLoader = new FontLoader();
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry('hello', {
+            font: font,
+            size: 0.5,
+            height: 0.2,
+            curveSegments: 5,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 4
+        });
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5, //minus bevelSize
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5, //minus bevelSize
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5 //minus bevelThickness
+        // );
+        textGeometry.center();
+
+        const textMaterial = new THREE.MeshMatcapMaterial();
+        textMaterial.matcap = matcapTexture;
+        // textMaterial.wireframe = true;
+        const text = new THREE.Mesh(textGeometry, textMaterial);
+        scene.add(text);
+
+        const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+        const donutMaterial = new THREE.MeshMatcapMaterial();
+        
+        // console.time('donuts');
+        for(let i = 0; i < 100; i++) {
+            donutMaterial.matcap = matcapTextureMetal;
+            const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI;
+            donut.rotation.y = Math.random() * Math.PI;
+
+            const scale = Math.random();
+            // donut.scale.x = scale;
+            // donut.scale.y = scale;
+            // donut.scale.z = scale;
+            donut.scale.set(scale, scale, scale);           
+            scene.add(donut);
+        }
+        // console.timeEnd('donuts');
+    }
+)
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
