28c12c657659d877d933e16ca133de50a590265f
Manon Oriol <oriol.manon@gmail.com>
2024-02-08 22:24:51 +0100

    p1 done

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/experience.js
@@ -0,0 +1,11 @@
+export default class Experience
+{
+    constructor(canvas) 
+    {
+        //Global access
+        window.experience = this
+
+        //Options
+        this.canvas = canvas
+    }
+}
